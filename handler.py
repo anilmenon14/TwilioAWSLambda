1b0257dc8963e4123505a1434c584f896752d053
anilmenon14 <anil.menon14@gmail.com>
2020-09-05 23:57:33 +0400

    Fixed issue with detail-type to ensure log streams are written in AWS Cloudwatch

--- handler.py
+++ handler.py
@@ -1,9 +1,7 @@
-import json
-
 
 def whatsappnotify(event, context):
+    print(event)
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "event": event
     }
-    """
